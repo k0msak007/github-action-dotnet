d7387a2b638cd86e4b34c58cecde2730d486929e
phromaupson <98792390+k0msak007@users.noreply.github.com>
2023-08-07 16:30:01 +0700

    debug

--- github-action-dotnet/WeatherForecast.cs
+++ github-action-dotnet/WeatherForecast.cs
@@ -8,7 +8,7 @@ namespace github_action_dotnet
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
-        public int? Days { get; set; };
+        public int? Days { get; set; }
 
         public string? Summary { get; set; }
     }
