84ab247d36bc98c127685d012fc1ae35e3a9e6ee
phromaupson <98792390+k0msak007@users.noreply.github.com>
2023-08-09 16:07:44 +0700

    debug

--- github-action-dotnet/Controllers/WeatherForecastController.cs
+++ github-action-dotnet/Controllers/WeatherForecastController.cs
@@ -23,7 +23,6 @@ namespace github_action_dotnet.Controllers
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
-                Date = DateTime.Now.AddDays(index),
                 TemperatureC = Random.Shared.Next(-20, 55),
                 Days = Random.Shared.Next(95, 99),
             })
